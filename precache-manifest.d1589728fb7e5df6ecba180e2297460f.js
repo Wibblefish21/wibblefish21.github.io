0465a5f703b7d86ab7304b3ca72883fa8b2bfaa5
Wibblefish21 <31337130+Wibblefish21@users.noreply.github.com>
2019-01-18 20:37:17 +1100

    Add files via upload

---
+++ precache-manifest.d1589728fb7e5df6ecba180e2297460f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "83b01e3c5eb30f404f7ebf47fa0534ed",
+    "url": "/portfolio/static/media/slick.83b01e3c.PNG"
+  },
+  {
+    "revision": "73cdd9ddbe4390a6de54",
+    "url": "/portfolio/static/css/main.43873bd4.chunk.css"
+  },
+  {
+    "revision": "df527a3f9533ba1b18e8",
+    "url": "/portfolio/static/js/1.df527a3f.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "63a92e5f90ddac1673418a7e0a0b704e",
+    "url": "/portfolio/static/media/menu.63a92e5f.svg"
+  },
+  {
+    "revision": "73cdd9ddbe4390a6de54",
+    "url": "/portfolio/static/js/main.73cdd9dd.chunk.js"
+  },
+  {
+    "revision": "93e07d64278fb918b67b4e4d0ddfe003",
+    "url": "/portfolio/static/media/blog1.93e07d64.png"
+  },
+  {
+    "revision": "25e4e8672b64a5278e705f888ce7d38a",
+    "url": "/portfolio/static/media/site2.25e4e867.PNG"
+  },
+  {
+    "revision": "61b741fc6347564ac3e4dc5d96f89d60",
+    "url": "/portfolio/static/media/travel.61b741fc.PNG"
+  },
+  {
+    "revision": "858eba05fc72598ad6bf6f928d6d85b5",
+    "url": "/portfolio/static/media/imggal.858eba05.png"
+  },
+  {
+    "revision": "a13ac2e0d5511e46d35a290609bd06f3",
+    "url": "/portfolio/index.html"
+  }
+];
